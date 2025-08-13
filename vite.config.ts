c6e900bb2a4a5a0c32f23104c52b33180031a023
lika2002 <likacaraja2002@gmail.com>
2025-08-13 11:00:16 +0100

    am optimizat

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/numele-repo-ului-tau/', // <- schimbă cu numele exact al repo-ului de pe GitHub
 })
