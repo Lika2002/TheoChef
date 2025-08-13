c535e3ff094625c91734907c8328da9a151cb9c1
lika2002 <likacaraja2002@gmail.com>
2025-08-13 11:14:24 +0100

    am optimizat ca sa se incarce site-ul

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/numele-repo-ului-tau/', // <- schimbă cu numele exact al repo-ului de pe GitHub
-})
+  base: "/TheoChef/",
+  plugins: [react()]
+});
